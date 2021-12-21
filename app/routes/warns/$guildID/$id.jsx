20e8bc2d9ecca9dbed7c112c40dc0b2832534976
Exerra <exerra@exerra.xyz>
2021-12-21 16:58:32 +0200

    Yes [deploy]

---
+++ app/routes/warns/$guildID/$id.jsx
@@ -0,0 +1,136 @@
+import { useCatch, Link, json, useLoaderData } from "remix";
+import axios from "axios";
+
+import stylesUrl from "~/styles/profile/$id.css";
+
+export let links = () => {
+    return null //[{ rel: "stylesheet", href: stylesUrl }];
+};
+
+let paramsObj = {}
+
+export let loader = async ({ params }) => {
+    if (params.id.match(/[0-9]{18}/) == false) {
+        throw new Response("Invalid regex", { status: 400 })
+        return
+    }
+
+    let data
+    let data2
+    let error
+
+    await fetch('https://cdn.exerra.xyz/karen/profile?' + new URLSearchParams({
+        id: params.id,
+        fetchUser: true,
+        includeWarns: true,
+        guildCaller: params.guildID
+    })).then(res => {
+        data = res
+    }).catch(err => {
+        throw new Response("Not Found", { status: 404 });
+    })
+
+    if (error) {
+        throw new Response("Not Found", { status: 404 });
+    }
+
+    return data
+
+}
+
+export default function ParamDemo() {
+    let data = useLoaderData();
+
+    let warnView = []
+
+
+    data.warns = data.warns.filter(item => item.guild == data.profile.file.caller);
+    data.warns.sort((a, b) => new Date(b.date) - new Date(a.date))
+
+    for (let i in data.warns) {
+        let warn = data.warns[i]
+        warnView.push(
+            <div key={warn.id}>
+                <h3 style={{ color: "#AD91FF" }}>{warn.id}</h3>
+                <p>Reason - {warn.reason}</p>
+                <p>Moderator - <Link to={`../../profile/${warn.moderator}`}>{warn.moderator}</Link></p>
+                <p>Guild - {warn.guild}</p>
+                <p>Date - {new Date(warn.date).toISOString().substring(0, 10)}</p>
+                <hr/>
+            </div>
+        )
+    }
+
+    return (
+        <div className="remix__page">
+            <h1>
+                Guild warns for <span style={{ color: "#AD91FF" }}>{data.username}#{data.discriminator}</span> {data.profile.rank !== '' ? `- ${data.profile.rank}` : ''}
+            </h1>
+
+            <hr/>
+
+            {warnView}
+        </div>
+    );
+}
+
+export function CatchBoundary({ error }) {
+    let caught = useCatch()
+
+    console.log(caught)
+
+    let message
+    if (caught.status == 404) {
+        message = (
+            <h1>
+                Profile for <i style={{color: "#AD91FF"}}>yes</i> could not be found
+            </h1>
+        )
+    }
+
+    return ({message})
+}
+
+export function ErrorBoundary({ error }) {
+    console.error(error);
+    return (
+        <div className="remix__page">
+            <main>
+                <h1>
+                    Warns could not be found
+                </h1>
+            </main>
+        </div>
+    );
+}
+
+export let meta = () => {
+    return {
+        title: "Karen Bot Profile",
+        description: "Karen Bot profile viewer"
+    };
+};
+/*
+let rankView
+if (data.profile.rank !== '') {
+    rankView = <h3>Rank = {data.profile.rank}</h3>
+}
+<main>
+    <h1>
+        Profile for <i style={{ color: "#AD91FF" }}>{data.id}</i> {data.profile.rank !== '' ? `- ${data.profile.rank}` : ''}
+    </h1>
+    {data.profile.website !== '' ? <h3>Website - <a href={data.profile.website}>{data.profile.website.replace(/(^\w+:|^)\/\//, '')}</a></h3> : ''}
+    {data.profile.email !== '' ? <h3>Email - <a href={`mailto:${data.profile.email}`}>{data.profile.email}</a></h3> : ''}
+    {data.profile.twitter !== '' ? <h3>Twitter - <a href={`https://twitter.com/${data.profile.twitter}`}>@{data.profile.twitter}</a></h3> : ''}
+
+
+    <h3>Description - {data.profile.description}</h3>
+
+    <h3>Gender - {data.profile.gender}</h3>
+
+    <h3>Birthday - {data.profile.birthday}</h3>
+
+    <h3>Country - {data.profile.country}</h3>
+
+    <h3>Languages - {data.profile.languages.replace(/\n/g, ", ").substr(2)}</h3>
+</main>*/
---
+++ app/routes/warns/$id.jsx
@@ -0,0 +1,153 @@
+import { useCatch, Link, json, useLoaderData } from "remix";
+import axios from "axios";
+
+import stylesUrl from "~/styles/profile/$id.css";
+
+export let links = () => {
+    return null //[{ rel: "stylesheet", href: stylesUrl }];
+};
+
+let paramsObj = {}
+
+export let loader = async ({ params }) => {
+    if (params.id.match(/[0-9]{18}/) == false) {
+        throw new Response("Invalid regex", { status: 400 })
+        return
+    }
+
+    let data
+    let data2
+    let error
+
+    await fetch('https://cdn.exerra.xyz/karen/profile?' + new URLSearchParams({
+        id: params.id,
+        fetchUser: true,
+        includeWarns: true
+    })).then(res => {
+        data = res
+    }).catch(err => {
+        throw new Response("Not Found", { status: 404 });
+    })
+
+    if (error) {
+        throw new Response("Not Found", { status: 404 });
+    }
+
+    return data
+
+}
+
+export default function ParamDemo() {
+    let data = useLoaderData();
+
+    let warnView = []
+
+    data.warns.sort((a, b) => new Date(b.date) - new Date(a.date))
+
+    for (let i in data.warns) {
+        let warn = data.warns[i]
+        warnView.push(
+            <div key={warn.id}>
+                <h3 style={{ color: "#AD91FF" }}>{warn.id}</h3>
+                <p>Reason - {warn.reason}</p>
+                <p>Moderator - <Link to={`../../profile/${warn.moderator}`}>{warn.moderator}</Link></p>
+                <p>Guild - {warn.guild}</p>
+                <p>Date - {new Date(warn.date).toISOString().substring(0, 10)}</p>
+                <hr/>
+            </div>
+        )
+    }
+
+    return (
+        <div className="remix__page">
+            <h1>
+                All warns for <span style={{ color: "#AD91FF" }}>{data.username}#{data.discriminator}</span> {data.profile.rank !== '' ? `- ${data.profile.rank}` : ''}
+            </h1>
+
+            <hr/>
+
+
+            {warnView}
+        </div>
+    );
+}
+
+export function CatchBoundary({ error }) {
+    let caught = useCatch()
+
+    console.log(caught)
+
+    let message
+    if (caught.status == 404) {
+        message = (
+            <h1>
+                Profile for <i style={{color: "#AD91FF"}}>yes</i> could not be found
+            </h1>
+        )
+    }
+
+    return ({message})
+}
+
+export function ErrorBoundary({ error }) {
+    console.error(error);
+    return (
+        <div className="remix__page">
+            <main>
+                <h1>
+                    Warns could not be found
+                </h1>
+                <hr></hr>
+                <h2>Possible causes</h2>
+                <h3>User has not created a profile</h3>
+                <p>If the user has not created a profile thru Karen Bot (or had it auto created when joining a guild with the bot in it), then it is not in our system. Tell them to create one.</p>
+                <h3>API has problems</h3>
+                <p>It is also possible that the API that has the profiles is either down or has a bug that prevents it from sending the profile. Rest assured, if this happens then the appropriate people have already been notified and are working on a fix.</p>
+            </main>
+            <form method="post" className={"remix__form"} onSubmit={e => {e.preventDefault()}}>
+                <h2>Want to try your luck again?</h2>
+                <p>
+                    <i>Discord ID's look like this <span style={{ color: "#AD91FF" }}>391878815263096833</span></i>
+                </p>
+                <input name="id" type="text" />
+                <button type="submit" onClick={() => {
+                    console.log(document.getElementsByTagName("input")[0].value)
+                    window.location.href = `/profile/${document.getElementsByTagName("input")[0].value}`
+                }} >
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export let meta = () => {
+    return {
+        title: "Karen Bot Profile",
+        description: "Karen Bot profile viewer"
+    };
+};
+/*
+let rankView
+if (data.profile.rank !== '') {
+    rankView = <h3>Rank = {data.profile.rank}</h3>
+}
+<main>
+    <h1>
+        Profile for <i style={{ color: "#AD91FF" }}>{data.id}</i> {data.profile.rank !== '' ? `- ${data.profile.rank}` : ''}
+    </h1>
+    {data.profile.website !== '' ? <h3>Website - <a href={data.profile.website}>{data.profile.website.replace(/(^\w+:|^)\/\//, '')}</a></h3> : ''}
+    {data.profile.email !== '' ? <h3>Email - <a href={`mailto:${data.profile.email}`}>{data.profile.email}</a></h3> : ''}
+    {data.profile.twitter !== '' ? <h3>Twitter - <a href={`https://twitter.com/${data.profile.twitter}`}>@{data.profile.twitter}</a></h3> : ''}
+
+
+    <h3>Description - {data.profile.description}</h3>
+
+    <h3>Gender - {data.profile.gender}</h3>
+
+    <h3>Birthday - {data.profile.birthday}</h3>
+
+    <h3>Country - {data.profile.country}</h3>
+
+    <h3>Languages - {data.profile.languages.replace(/\n/g, ", ").substr(2)}</h3>
+</main>*/
