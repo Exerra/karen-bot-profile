3ddab3eca22a3c078a602b608d08e176c5ca4bae
Exerra <exerra@exerra.xyz>
2021-12-31 01:00:04 +0200

    Button material-ied [deploy]

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -71,7 +71,7 @@ export default function Index() {
           <i>Discord ID's look like this <span style={{ color: "#AD91FF" }}>391878815263096833</span></i>
         </p>
         <input name="id" type="text" />
-        <button type="submit" onClick={() => {
+        <button className={"idButton"} type="submit" onClick={() => {
           console.log(document.getElementsByTagName("input")[0].value)
           window.location.href = `/profile/${document.getElementsByTagName("input")[0].value}`
         }} >
