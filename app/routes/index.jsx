0f746b38410e7613a02d0a0f6eea47543b9f7861
Exerra <exerra@exerra.xyz>
2021-12-31 02:39:00 +0200

    Removed useless loader [deploy]

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,45 +1,5 @@
 import { useLoaderData, json, Link, Form, redirect } from "remix";
 
-// Loaders provide data to components and are only ever called on the server, so
-// you can connect to a database or run any server side code you want right next
-// to the component that renders it.
-// https://remix.run/api/conventions#loader
-export let loader = () => {
-  let data = {
-    resources: [
-      {
-        name: "Remix Docs",
-        url: "https://remix.run/docs"
-      },
-      {
-        name: "React Router Docs",
-        url: "https://reactrouter.com/docs"
-      },
-      {
-        name: "Remix Discord",
-        url: "https://discord.gg/VBePs6d"
-      }
-    ],
-    demos: [
-      {
-        to: "demos/actions",
-        name: "Actions"
-      },
-      {
-        to: "demos/about",
-        name: "Nested Routes, CSS loading/unloading"
-      },
-      {
-        to: "demos/params",
-        name: "URL Params and Error Boundaries"
-      }
-    ]
-  };
-
-  // https://remix.run/api/remix#json
-  return json(data);
-};
-
 // https://remix.run/api/conventions#meta
 export let meta = () => {
   return {
@@ -50,7 +10,6 @@ export let meta = () => {
 
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
-  let data = useLoaderData();
 
   return (
     <div className="remix__page">
