41b58f9dfef6c1be06f7690ca02e941b75cdd5da
Exerra <exerra@exerra.xyz>
2021-12-31 18:17:16 +0200

    Local lottie player [deploy]

--- app/modules/lottie.jsx
+++ app/modules/lottie.jsx
@@ -3,8 +3,8 @@ export default function lottieEmpty() {
 		<div key={"lottie"}>
 			<h3>Once upon a time, there was nothing...</h3>
 			<br />
-			<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-			<lottie-player src="https://assets3.lottiefiles.com/packages/lf20_ydo1amjm.json"  background="transparent"  speed="1"  loop  autoplay></lottie-player>
+			<script src="/lottie/lottie-player.js"></script>
+			<lottie-player src="/lottie/empty.json"  background="transparent"  speed="1"  loop  autoplay></lottie-player>
 		</div>
 	)
 }
