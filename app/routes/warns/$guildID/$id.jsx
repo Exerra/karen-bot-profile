a057c3ce5d0fec02d424430e4e68e3ee2dc662e3
Exerra <exerra@exerra.xyz>
2021-12-31 02:13:17 +0200

    Lottie 2 [deploy]

--- app/routes/warns/$guildID/$id.jsx
+++ app/routes/warns/$guildID/$id.jsx
@@ -61,6 +61,17 @@ export default function ParamDemo() {
         )
     }
 
+	if (data.warns.length == 0) {
+		warnView.push(
+			<>
+				<h3>Once upon a time, there was nothing...</h3>
+				<br />
+				<script key={"smgh"} src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+				<lottie-player key={"luuti"} src="https://assets3.lottiefiles.com/packages/lf20_ydo1amjm.json"  background="transparent"  speed="1"  loop  autoplay></lottie-player>
+			</>
+		)
+	}
+
     return (
         <div className="">
             <br />
